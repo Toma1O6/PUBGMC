5d89a6eaa8cdb5ae644fc410e13fc00aea6d1211
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-08-24 14:48:28 +0200

    Create GuiLootSetup.java

---
+++ src/main/java/com/toma/pubgmc/client/gui/GuiLootSetup.java
@@ -0,0 +1,17 @@
+package com.toma.pubgmc.client.gui;
+
+import com.toma.pubgmc.common.capability.IWorldData;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+
+public class GuiLootSetup extends GuiScreen {
+
+    public GuiLootSetup(IWorldData lootData) {
+        if(mc.player.getPermissionLevel() < 4) {
+            mc.displayGuiScreen(null);
+            mc.player.sendMessage(new TextComponentString(TextFormatting.RED + "You don't have permission to edit loot data"));
+            return;
+        }
+    }
+}
