5f151ef3f06effe1bda5b9f4b6dfa7319756d403
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-11-05 18:35:15 +0100

    update death messages

--- src/main/java/com/toma/pubgmc/init/DamageSourceGun.java
+++ src/main/java/com/toma/pubgmc/init/DamageSourceGun.java
@@ -7,20 +7,20 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EntityDamageSourceIndirect;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
 
 import javax.annotation.Nullable;
 import java.util.Random;
 
 public class DamageSourceGun extends EntityDamageSourceIndirect {
-    private static final String[] HS_MESSAGES = new String[]{"headshotted", "shot right into head", "blew head of"};
-    private static final String[] DEATH_MESSAGES = {"shot", "killed", "shredded", "sniped", "obliterated"};
+
     private final Random rand = new Random();
     private ItemStack weapon;
     private Entity source;
     private Entity indirect;
     private boolean headshot;
 
-    public DamageSourceGun(String damageType, Entity source, @Nullable Entity indirect, ItemStack weapon, boolean headshot) {
+    public DamageSourceGun(String damageType, Entity source, Entity indirect, ItemStack weapon, boolean headshot) {
         super(damageType, source, indirect);
         this.weapon = weapon;
         this.source = source;
@@ -28,16 +28,13 @@ public class DamageSourceGun extends EntityDamageSourceIndirect {
         this.headshot = headshot;
     }
 
-    private String[] getMessageType() {
-        return headshot ? HS_MESSAGES : DEATH_MESSAGES;
-    }
-
     @Override
     public ITextComponent getDeathMessage(EntityLivingBase entityLivingBaseIn) {
         GunBase gun = (GunBase) weapon.getItem();
-        String[] s = getMessageType();
-        int i = rand.nextInt(s.length);
-        return new TextComponentString(source.getName() + " " + s[i] + " " + indirect.getName() + " using " + weapon.getDisplayName());
+        if(wasHeadshot()) {
+            return new TextComponentTranslation("death.gun.headshot", source.getName(), indirect.getName(), weapon.getDisplayName());
+        }
+        return new TextComponentTranslation("death.gun", source.getName(), indirect.getName(), weapon.getDisplayName());
     }
 
     @Nullable
