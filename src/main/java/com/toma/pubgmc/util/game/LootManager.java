b20c25a6ceef12fe8e5fd3705fb631db4e5770d9
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-11-16 15:05:18 +0100

    Create LootManager.java

---
+++ src/main/java/com/toma/pubgmc/util/game/LootManager.java
@@ -0,0 +1,15 @@
+package com.toma.pubgmc.util.game;
+
+import net.minecraft.world.World;
+
+/**
+ * WIP
+ */
+public class LootManager {
+
+    private final World world;
+
+    public LootManager(World world) {
+        this.world = world;
+    }
+}
