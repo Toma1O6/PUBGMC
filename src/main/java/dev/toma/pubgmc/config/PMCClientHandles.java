a7d25ee6228c5a1ab47fce4f628841c498e5e659
Toma <novotny.tom96@gmail.com>
2021-04-22 20:11:14 +0200

    1.7.0 update

--- src/main/java/dev/toma/pubgmc/config/PMCClientHandles.java
+++ src/main/java/dev/toma/pubgmc/config/PMCClientHandles.java
@@ -22,6 +22,6 @@ public class PMCClientHandles extends ClientHandles.DefaultClientHandles {
 
     @Override
     public int getTextColor() {
-        return 0xCBCBCB;
+        return 0xFFFFFF;
     }
 }
