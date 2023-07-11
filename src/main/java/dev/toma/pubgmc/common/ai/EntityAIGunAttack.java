d35c425689146e6ca4caaf2786d167feca914c22
Toma <novotny.tom96@gmail.com>
2023-07-11 10:03:25 +0200

    Improved AI task for shooting

--- src/main/java/dev/toma/pubgmc/common/ai/EntityAIGunAttack.java
+++ src/main/java/dev/toma/pubgmc/common/ai/EntityAIGunAttack.java
@@ -17,7 +17,6 @@ public class EntityAIGunAttack extends EntityAIBase {
     private static final int[] EFFECTIVE_RANGE_TABLE = {40, 20, 10, 20, 40, 60, 60};
     private final EntityAIPlayer aiPlayer;
     private int timeWatching;
-    private EntityLivingBase target;
 
     private int shootCooldown;
     private int firedTotal;
@@ -30,51 +29,49 @@ public class EntityAIGunAttack extends EntityAIBase {
 
     @Override
     public boolean shouldExecute() {
-        if (this.aiPlayer.hasNoWeapon()) {
+        if (aiPlayer.hasNoWeapon()) {
             return false;
         }
-        this.target = this.aiPlayer.getAttackTarget();
-        return target != null && target.isEntityAlive();
+        EntityLivingBase target = aiPlayer.getAttackTarget();
+        return target != null && target.isEntityAlive() && !aiPlayer.hasNoWeapon();
     }
 
     @Override
     public boolean shouldContinueExecuting() {
-        return this.shouldExecute() || !this.aiPlayer.getNavigator().noPath() || this.aiPlayer.hasNoWeapon();
+        return this.shouldExecute();
     }
 
     @Override
     public void resetTask() {
         timeWatching = 0;
-        target = null;
         shootCooldown = 10;
-        firedTotal = 0;
         firedBurst = 0;
     }
 
     @Override
     public void updateTask() {
-        if (target == null) return;
-        double distanceToTarget = Math.sqrt(this.aiPlayer.getDistanceSq(this.target));
-        boolean flag = this.aiPlayer.getEntitySenses().canSee(this.target);
+        EntityLivingBase target = aiPlayer.getAttackTarget();
+        double distanceToTarget = Math.sqrt(aiPlayer.getDistanceSq(target));
+        boolean flag = aiPlayer.getEntitySenses().canSee(target);
         if (flag) {
-            this.timeWatching++;
+            timeWatching++;
         } else timeWatching = 0;
-        if (this.aiPlayer.hasNoWeapon()) {
+        if (aiPlayer.hasNoWeapon()) {
             return;
         }
         ItemStack stack = aiPlayer.getHeldItemMainhand();
         if (stack.getItem() instanceof GunBase) {
             GunBase gun = (GunBase) stack.getItem();
             int tableIndex = gun.getGunType().ordinal();
-            if (distanceToTarget <= EFFECTIVE_RANGE_TABLE[tableIndex] * 3.0 && this.timeWatching >= 40) {
-                this.aiPlayer.getNavigator().clearPath();
-            } else this.aiPlayer.getNavigator().tryMoveToEntityLiving(this.target, 1.0D);
-            this.aiPlayer.getLookHelper().setLookPositionWithEntity(this.target, 30, 30);
-            if (--this.shootCooldown <= 0) {
+            if (distanceToTarget <= EFFECTIVE_RANGE_TABLE[tableIndex] * 3.0) {
+                aiPlayer.getNavigator().clearPath();
+            } else aiPlayer.getNavigator().tryMoveToEntityLiving(target, 1.0D);
+            aiPlayer.getLookHelper().setLookPositionWithEntity(target, 30, 30);
+            if (timeWatching >= 15 && --shootCooldown <= 0) {
                 if (!flag) {
                     return;
                 }
-                this.shoot(gun, stack, distanceToTarget);
+                shoot(gun, stack, distanceToTarget);
             }
         }
     }
@@ -83,32 +80,32 @@ public class EntityAIGunAttack extends EntityAIBase {
         if (distanceToTarget > EFFECTIVE_RANGE_TABLE[gun.getGunType().ordinal()] * 10) {
             return;
         }
-        ++this.firedTotal;
+        ++firedTotal;
         boolean burstFire = distanceToTarget < EFFECTIVE_RANGE_TABLE[gun.getGunType().ordinal()];
         int shotAmount = gun.getGunType() == GunBase.GunType.SHOTGUN ? 8 : 1;
         ItemMuzzle muzzle = gun.getAttachment(AttachmentType.MUZZLE, stack);
         boolean isSilenced = muzzle != null && muzzle.isSilenced();
         SoundEvent event = isSilenced ? gun.getGunSilencedSound() : gun.getGunSound();
         float volume = isSilenced ? gun.getGunSilencedVolume() : gun.getGunVolume();
-        PacketHandler.sendToDimension(new PacketDelayedSound(event, volume, this.aiPlayer.posX, this.aiPlayer.posY, this.aiPlayer.posZ), this.aiPlayer.dimension);
+        PacketHandler.sendToDimension(new PacketDelayedSound(event, volume, aiPlayer.posX, aiPlayer.posY, aiPlayer.posZ), aiPlayer.dimension);
         for (int i = 0; i < shotAmount; i++) {
-            EntityBullet bullet = new EntityBullet(this.aiPlayer.world, this.aiPlayer, gun);
+            EntityBullet bullet = new EntityBullet(aiPlayer.world, aiPlayer, gun);
             bullet.setPosition(bullet.posX, bullet.posY - 0.5, bullet.posZ);
             if (shotAmount > 1) {
                 double d0 = distanceToTarget / 150;
                 bullet.motionY += d0;
             }
-            this.aiPlayer.world.spawnEntity(bullet);
+            aiPlayer.world.spawnEntity(bullet);
         }
-        this.shootCooldown = gun.getFireRate() + (gun.getGunType() == GunBase.GunType.DMR ? 10 : 0) + aiPlayer.getRNG().nextInt(5);
+        shootCooldown = gun.getFireRate() + (gun.getGunType() == GunBase.GunType.DMR ? 10 : 0) + aiPlayer.getRNG().nextInt(5);
         if (burstFire && ++firedBurst > 3) {
             firedBurst = 0;
-            this.shootCooldown = 15;
+            shootCooldown = 15;
         }
         if (firedTotal >= gun.getWeaponAmmoLimit(stack)) {
-            this.firedTotal = 0;
-            this.firedBurst = 0;
-            this.shootCooldown = gun.getReloadTime(stack);
+            firedTotal = 0;
+            firedBurst = 0;
+            shootCooldown = gun.getReloadTime(stack);
         }
     }
 }
