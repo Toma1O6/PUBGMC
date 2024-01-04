2fe23e5445a14b43272703f1b9c2f83ee8ae21ae
Toma <novotny.tom96@gmail.com>
2024-01-04 19:51:54 +0100

    Added support for multiword command arguments

--- src/main/java/dev/toma/pubgmc/common/commands/core/CommandContext.java
+++ src/main/java/dev/toma/pubgmc/common/commands/core/CommandContext.java
@@ -9,9 +9,7 @@ import net.minecraft.command.WrongUsageException;
 import net.minecraft.server.MinecraftServer;
 
 import javax.annotation.Nullable;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public final class CommandContext {
 
@@ -32,7 +30,9 @@ public final class CommandContext {
     }
 
     public static CommandContext parse(CommandTree tree, MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        ArgumentReader reader = new ArgumentReader(args);
+        String cmdText = String.join(" ", args);
+        CommandStringReader commandReader = new CommandStringReader(cmdText);
+        ArgumentReader reader = new ArgumentReader(commandReader.toArgumentArray());
         Map<String, Object> argumentMap = new HashMap<>();
         CommandNode node = null;
         CommandNodeExecutor executor = tree.getDefaultExecutor();
@@ -99,5 +99,79 @@ public final class CommandContext {
 
     int getPermissionLevel() {
         return permissionLevel;
+    }
+
+    public static final class CommandStringReader {
+
+        private final String cmd;
+        private int pos;
+        private boolean multiWord;
+
+        public CommandStringReader(String cmd) {
+            this.cmd = cmd;
+        }
+
+        public String[] toArgumentArray() throws CommandException {
+            List<String> list = new ArrayList<>();
+            String text;
+            while ((text = tryReadArgument()) != null) {
+                if (!text.isEmpty()) {
+                    list.add(text);
+                }
+            }
+            return list.toArray(new String[0]);
+        }
+
+        public String tryReadArgument() throws CommandException {
+            StringBuilder builder = new StringBuilder();
+            if (pos == cmd.length()) {
+                return null;
+            }
+            while (true) {
+                char character = read();
+                if (character == '"') {
+                    if (multiWord) {
+                        char following = peek();
+                        if (following != ' ' && following != Character.MIN_VALUE) {
+                            // Not empty space after closing quote
+                            throw new SyntaxErrorException("Whitespace character must be present after closing quote");
+                        }
+                        multiWord = false;
+                        break;
+                    } else {
+                        multiWord = true;
+                    }
+                } else if (character == ' ') {
+                    if (multiWord) {
+                        builder.append(character);
+                    } else {
+                        break;
+                    }
+                } else if (character == Character.MIN_VALUE) {
+                    return builder.toString();
+                } else {
+                    builder.append(character);
+                }
+            }
+            String result = builder.toString();
+            if (result.isEmpty()) {
+                throw new SyntaxErrorException("Argument value cannot be empty");
+            }
+            return result;
+        }
+
+        public char read() {
+            if (pos == cmd.length()) {
+                return Character.MIN_VALUE;
+            }
+            return cmd.charAt(pos++);
+        }
+
+        public char peek() {
+            if ((pos + 1) >= cmd.length()) {
+                return Character.MIN_VALUE;
+            }
+            return cmd.charAt(pos + 1);
+        }
     }
 }
