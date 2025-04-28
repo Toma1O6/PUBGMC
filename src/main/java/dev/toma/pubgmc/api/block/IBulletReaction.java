295f64ed2bac877d965ce41e8b88c77786e1aa1b
XiaoColorful <XColor_ful@outlook.com>
2025-04-28 22:33:36 +0800

    Prepare for more bullet interaction

--- src/main/java/dev/toma/pubgmc/api/block/IBulletReaction.java
+++ src/main/java/dev/toma/pubgmc/api/block/IBulletReaction.java
@@ -2,9 +2,11 @@ package dev.toma.pubgmc.api.block;
 
 import dev.toma.pubgmc.common.entity.EntityBullet;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.entity.Entity;
+
+import javax.annotation.Nullable;
 
 /**
  * Allows additional interaction when impacted by bullet entity
@@ -18,16 +20,17 @@ public interface IBulletReaction {
      * Handles projectile impact interaction
      * @param bullet Impacted bullet entity
      * @param hit Exact coordinates of hit on this block
-     * @param pos Position of this block
+     * @param state Interaction block state
+     * @param entity Intraction entity
      */
-    void onHit(EntityBullet bullet, Vec3d hit, BlockPos pos);
+    void onHit(EntityBullet bullet, Vec3d hit, @Nullable IBlockState state, @Nullable Entity entity);
 
     /**
-     * Determines whether {@link IBulletReaction#onHit(EntityBullet, Vec3d, BlockPos)} can be called for this specific blockstate.
+     * Determines whether {@link IBulletReaction#onHit(EntityBullet, Vec3d, IBlockState, Entity)} can be called for this specific blockstate.
      * @param world Interaction world
-     * @param pos Interaction block pos
      * @param state Interaction block state
+     * @param entity Intraction entity
      * @return If further processing of this interface is allowed
      */
-    boolean allowBulletInteraction(World world, BlockPos pos, IBlockState state);
+    boolean allowBulletInteraction(World world, @Nullable IBlockState state, @Nullable Entity entity);
 }
